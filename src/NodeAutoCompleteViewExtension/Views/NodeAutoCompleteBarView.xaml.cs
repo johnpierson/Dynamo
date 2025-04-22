4c0d5b6fc1bf114719ef6dee5588313c46b4c6f4
john pierson <sixtysecondrevit@gmail.com>
2025-04-22 07:16:56 -0600

    Cleanup code for locking undo/redo

--- src/NodeAutoCompleteViewExtension/Views/NodeAutoCompleteBarView.xaml.cs
+++ src/NodeAutoCompleteViewExtension/Views/NodeAutoCompleteBarView.xaml.cs
@@ -138,6 +138,7 @@ namespace Dynamo.NodeAutoComplete.Views
             Dispatcher.BeginInvoke(new Action(() =>
             {
                 ViewModel?.DeleteTransientNodes();
+                ViewModel?.ToggleUndoRedoLocked(false);
             }), DispatcherPriority.Loaded);
             
             Close();
